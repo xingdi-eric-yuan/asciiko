a8aeaeab89713805c514d0eeafd83724055bf3ed
Xingdi (Eric) Yuan <eric.yuan@maluuba.com>
2018-07-05 11:22:21 -0400

    remove video2charid file, merge into img2charid

--- README.md
+++ README.md
@@ -14,10 +14,13 @@ It will take some time if you run the first time, asciiko will preprocess and pa
 ## To Run
 * In `config/config.yaml`, enable CUDA if you have access to it, also specify which split of ETL data are you using.
 * To train a model, run `python train.py -c config/`.
-* To generate ascii strings from an image, run `python img2charid.py`, it will generate a `.json` file;
-* To generate ascii strings from a video, run `python utils/get_video_frames.py`, to get all frames of a video into a folder, then run `python video2charid.py`, it will generate a `.json` file.
+* To generate ascii strings from an image, modify and run `python img2charid.py`, it will generate a `.json` file;
+* To generate ascii strings from a video, run `python utils/get_video_frames.py`, to get all frames of a video into a folder, then modify and run `python img2charid.py`, it will generate a `.json` file.
 * To render the ascii arts, run `python char_classifier/renderer.py`, with your `.json` file specified inside.
 * a pretrained model is provided in `saved_models/`.
+
+## Image Processing
+Image processing is an important step before generating ascii arts, and the performance of the latter steps are highly relying on how good the images are processed, i.e., how good the edges are extracted. To our knowledge there's no 'panacea' for all input images/videos, so depending on which specific image or video you are using, sometimes you need to massage your edge detector carefully. Thanks to OpenCV, one can do some standard image processing tricks in one line of python code. We provide code to help tweaking your edge detector in `utils/`.
 
 ## Demo
 * <p align=center><img width="70%" src="demo.jpg" /></p>
--- img2charid.py
+++ img2charid.py
@@ -1,6 +1,7 @@
 import cv2
 import json
 import torch
+from tqdm import tqdm
 import numpy as np
 from helpers.inference import load_id2label, load_model, predict
 classifier_img_size = 20
@@ -15,73 +16,129 @@ def binarize(img):
     return imgf
 
 
-img = cv2.imread('test_images/tsubasa_2.png', 0)
-img = binarize(img)
-height, width = img.shape[0], img.shape[1]
-print(height, width)
-
-h = height // classifier_img_size
-if height % classifier_img_size > 0:
-    h += 1
-h *= classifier_img_size
-
-w = width // classifier_img_size
-if width % classifier_img_size > 0:
-    w += 1
-w *= classifier_img_size
-w, h = w * 2, h * 2
-
-height, width = h, w
-print(height, width)
-img_resize = cv2.resize(img, (width, height))
-img_resize = img_resize.astype('uint8')
-img_resize = binarize(img_resize)
-# invert colors cuz in the test images, edges are in black
-img_resize = 255 - img_resize
-n_vert, n_hori = height // classifier_img_size, width // classifier_img_size
-
-subimgs = []
-for h in range(n_vert):
-    for w in range(n_hori):
-        _img = img_resize[h * classifier_img_size: (h + 1) * classifier_img_size, w * classifier_img_size: (w + 1) * classifier_img_size]
-        subimgs.append(_img)
-
-# heuristics: subimage without any value --> SPACE
-non_zeros = []
-non_zeros_idx = []
-non_ratio = 0.1
-for i, _img in enumerate(subimgs):
-    _tmp = _img.astype('float32') / 255.0
-    if np.sum(_tmp) <= _img.shape[0] * _img.shape[1] * non_ratio:
-        continue
-    non_zeros.append(_img)
-    non_zeros_idx.append(i)
-
-x = np.stack(non_zeros, 0)  # non_zeros x 20 x 20
-# 0-255 --> 0.0-1.0
-x = x.astype('float32') / 255.0
-x = x.reshape((x.shape[0], 1) + x.shape[1:])
-if use_cuda:
-    x = torch.autograd.Variable(torch.from_numpy(x).type(torch.FloatTensor).cuda())
-else:
-    x = torch.autograd.Variable(torch.from_numpy(x).type(torch.FloatTensor))
-
-# load model
-model = load_model(model_checkpoint_path, use_cuda)
-
-y = []
-number_batch = (x.size(0) + batch_size - 1) // batch_size
-for b in range(number_batch):
-    batch_x = x[b * batch_size: (b + 1) * batch_size]
-    batch_y = predict(model, batch_x, use_cuda, etl=etl)
-    y += batch_y.tolist()
-
-id2label = load_id2label(etl=etl)
-res = [['SP' for j in range(n_hori)] for i in range(n_vert)]
-labels = [id2label[item] for item in y]
-for _l, idx in zip(labels, non_zeros_idx):
-    res[idx // n_hori][idx % n_hori] = _l
+def get_edges(img):
+    img = cv2.equalizeHist(img)
+    img = cv2.Canny(img, 100, 200)
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    return img
 
 
-with open("tmp.json", "w") as text_file:
-    json.dump(res, text_file)
+def single_image_to_charids(img, new_img_w, new_img_h, model, id2label):
+    assert new_img_w % classifier_img_size == 0
+    assert new_img_h % classifier_img_size == 0
+    img = binarize(img)
+    height, width = new_img_h, new_img_w
+    # riseze
+    img_resize = cv2.resize(img, (new_img_w, new_img_h))
+    img_resize = img_resize.astype('uint8')
+    img_resize = binarize(img_resize)
+    n_vert, n_hori = height // classifier_img_size, width // classifier_img_size
+
+    subimgs = []
+    for h in range(n_vert):
+        for w in range(n_hori):
+            _img = img_resize[h * classifier_img_size: (h + 1) * classifier_img_size, w * classifier_img_size: (w + 1) * classifier_img_size]
+            subimgs.append(_img)
+
+    # heuristics: subimage without any value --> SPACE
+    non_zeros = []
+    non_zeros_idx = []
+    non_ratio = 0.1
+    for i, _img in enumerate(subimgs):
+        _tmp = _img.astype('float32') / 255.0
+        if np.sum(_tmp) <= _img.shape[0] * _img.shape[1] * non_ratio:
+            continue
+        non_zeros.append(_img)
+        non_zeros_idx.append(i)
+
+    x = np.stack(non_zeros, 0)  # non_zeros x 20 x 20
+    # 0-255 --> 0.0-1.0
+    x = x.astype('float32') / 255.0
+    x = x.reshape((x.shape[0], 1) + x.shape[1:])
+    if use_cuda:
+        x = torch.autograd.Variable(torch.from_numpy(x).type(torch.FloatTensor).cuda())
+    else:
+        x = torch.autograd.Variable(torch.from_numpy(x).type(torch.FloatTensor))
+
+    y = []
+    number_batch = (x.size(0) + batch_size - 1) // batch_size
+    for b in range(number_batch):
+        batch_x = x[b * batch_size: (b + 1) * batch_size]
+        batch_y = predict(model, batch_x, use_cuda, etl=etl)
+        y += batch_y.tolist()
+
+    res = [['SP' for j in range(n_hori)] for i in range(n_vert)]
+    labels = [id2label[item] for item in y]
+    for _l, idx in zip(labels, non_zeros_idx):
+        res[idx // n_hori][idx % n_hori] = _l
+    return res
+
+
+def get_charids_from_bunch_of_edge_frames():
+    # for nichijou_op.mp4
+    # load model
+    model = load_model(model_checkpoint_path, use_cuda)
+    ori_width, ori_height = 1280, 720
+    id2label = load_id2label(etl=etl)
+    # 2156 frames
+    frames = []
+    for i in tqdm(range(2156)):
+        img = cv2.imread("nichijou/frame" + str(i) + ".png", 0)
+        # originally 1280 x 720
+        tmp = single_image_to_charids(img, ori_width * 3, ori_height * 3, model, id2label)
+        frames += [tmp]
+
+    with open("nichijou_ascii_strings_x3.json", "w") as text_file:
+        json.dump(frames, text_file)
+
+
+def get_new_image_size(img):
+    # because both
+    # img.width % classifier_img_size and
+    # img.height % classifier_img_size should be 0
+    height, width = img.shape[0], img.shape[1]
+    h = height // classifier_img_size
+    if height % classifier_img_size > 0:
+        h += 1
+    h *= classifier_img_size
+
+    w = width // classifier_img_size
+    if width % classifier_img_size > 0:
+        w += 1
+    w *= classifier_img_size
+    return w, h
+
+
+def get_charids_from_raw_image():
+    img = cv2.imread('test_images/lenna.png', 0)
+    img = get_edges(img)
+
+    model = load_model(model_checkpoint_path, use_cuda)
+    id2label = load_id2label(etl=etl)
+    new_img_w, new_img_h = get_new_image_size(img)
+    # to + resolution, enable the following line
+    new_img_w, new_img_h = new_img_w * 20, new_img_h * 20
+    charids = single_image_to_charids(img, new_img_w, new_img_h, model, id2label)
+    with open("tmp.json", "w") as text_file:
+        json.dump(charids, text_file)
+
+
+def get_charids_from_edge_image():
+    img = cv2.imread('test_images/tsubasa_2.png', 0)
+    img = binarize(img)
+    # invert colors cuz in the test images, edges are black while background is white
+    img = 255 - img
+    model = load_model(model_checkpoint_path, use_cuda)
+    id2label = load_id2label(etl=etl)
+    new_img_w, new_img_h = get_new_image_size(img)
+    # to + resolution, enable the following line
+    new_img_w, new_img_h = new_img_w * 4, new_img_h * 4
+    charids = single_image_to_charids(img, new_img_w, new_img_h, model, id2label)
+    with open("tmp.json", "w") as text_file:
+        json.dump(charids, text_file)
+
+
+get_charids_from_raw_image()
+# get_charids_from_edge_image()
+# get_charids_from_bunch_of_edge_frames()
